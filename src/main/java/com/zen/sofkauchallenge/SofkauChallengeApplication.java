67517642adc82af1852a7d516b6d0a5987e5d558
David <depch47@gmail.com>
2022-05-04 09:13:00 -0500

    starter files

---
+++ src/main/java/com/zen/sofkauchallenge/SofkauChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.zen.sofkauchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofkauChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SofkauChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zen/sofkauchallenge/SofkauChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zen.sofkauchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofkauChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
