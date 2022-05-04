31cbf6adea0f1a63bc5941ee551f443b23fa14cc
David <depch47@gmail.com>
2022-05-04 15:09:40 -0500

    FIXED---CategoryController add the requestMapping string

--- src/main/java/com/zen/sofkauchallenge/controller/CategoryController.java
+++ src/main/java/com/zen/sofkauchallenge/controller/CategoryController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("api/v1/categories")
 public class CategoryController {
 
     private final CategoryServiceImplementation categoryService;
