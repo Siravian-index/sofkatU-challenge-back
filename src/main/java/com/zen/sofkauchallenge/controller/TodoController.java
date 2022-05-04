251c1f7727ec7f9fb43465e5231cce0c47afa330
David <depch47@gmail.com>
2022-05-04 15:03:05 -0500

    ADD---TodoController

---
+++ src/main/java/com/zen/sofkauchallenge/controller/TodoController.java
@@ -0,0 +1,36 @@
+package com.zen.sofkauchallenge.controller;
+
+import com.zen.sofkauchallenge.entity.Todo;
+import com.zen.sofkauchallenge.service.TodoServiceImplementation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+
+@RestController
+@RequestMapping("api/v1/todos")
+public class TodoController {
+
+    private final TodoServiceImplementation todoService;
+
+    @Autowired
+    public TodoController(TodoServiceImplementation todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    public Todo createTodo(@RequestBody Todo todo) {
+        return todoService.addTodo(todo);
+    }
+
+    @PutMapping
+    public Todo updateTodo(@RequestBody Todo todo) {
+        return todoService.updateTodo(todo);
+    }
+
+    @DeleteMapping("/{id}")
+    public Boolean deleteTodo(@PathVariable Long id) {
+        return todoService.deleteTodo(id);
+    }
+
+}
