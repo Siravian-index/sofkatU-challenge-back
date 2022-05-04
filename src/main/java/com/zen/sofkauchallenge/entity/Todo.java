7636b8b85304a0de42939bffba8c554b3404b3dd
David <depch47@gmail.com>
2022-05-04 11:23:49 -0500

    ADD---Todo entity with annotations

--- src/main/java/com/zen/sofkauchallenge/entity/Todo.java
+++ src/main/java/com/zen/sofkauchallenge/entity/Todo.java
@@ -1,9 +1,18 @@
 package com.zen.sofkauchallenge.entity;
 
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Data
 public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String title;
     private Boolean isDone;
-
+    private Long categoryFK;
 }
