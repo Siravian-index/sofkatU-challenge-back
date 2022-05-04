368ba733ad17175ef04653c8e7f17b61c7f0b1a0
David <depch47@gmail.com>
2022-05-04 15:07:35 -0500

    ADD---CategoryController

---
+++ src/main/java/com/zen/sofkauchallenge/controller/CategoryController.java
@@ -0,0 +1,36 @@
+package com.zen.sofkauchallenge.controller;
+
+import com.zen.sofkauchallenge.entity.Category;
+import com.zen.sofkauchallenge.service.CategoryServiceImplementation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+public class CategoryController {
+
+    private final CategoryServiceImplementation categoryService;
+
+    @Autowired
+    public CategoryController(CategoryServiceImplementation categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+
+    @PostMapping
+    public Category createCategory(@RequestBody Category category) {
+        return categoryService.createCategory(category);
+    }
+
+    @DeleteMapping("/{id}")
+    public Boolean deleteCategory(@PathVariable Long id) {
+        return categoryService.deleteCategory(id);
+    }
+
+}
