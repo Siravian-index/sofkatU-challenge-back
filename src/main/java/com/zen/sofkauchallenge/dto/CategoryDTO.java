8547f0f7135592cf5e2c5142e1a018f4bc56f220
David <depch47@gmail.com>
2022-05-05 19:59:32 -0500

    ADD---CategoryDTO implementation

--- src/main/java/com/zen/sofkauchallenge/dto/CategoryDTO.java
+++ src/main/java/com/zen/sofkauchallenge/dto/CategoryDTO.java
@@ -1,10 +1,12 @@
 package com.zen.sofkauchallenge.dto;
 
 import com.zen.sofkauchallenge.entity.Todo;
+import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
 public class CategoryDTO {
     private Long id;
     private String title;
--- src/main/java/com/zen/sofkauchallenge/dto/TodoDTO.java
+++ src/main/java/com/zen/sofkauchallenge/dto/TodoDTO.java
@@ -1,5 +1,8 @@
 package com.zen.sofkauchallenge.dto;
 
+import lombok.Data;
+
+@Data
 public class TodoDTO {
     private Long id;
     private String title;
